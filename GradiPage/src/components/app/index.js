daeb6cd937155449e72174e6f0287bc0daa4ec50
Eduar Sanchez <estebansr1998@gmail.com>
2020-03-04 15:22:55 -0500

    added helpers and installing axios

--- GradiPage/src/components/app/index.js
+++ GradiPage/src/components/app/index.js
@@ -1,17 +1,21 @@
 import * as css from './index.css'
+import {getData} from "../../helpers/services"
 
 export default class App {
     constructor (elem) {
       if (!elem) return
       this.elem = elem
+      let dataList = getData()
+      console.log(dataList)
     }
 
     render () {
-        if (this.elem) this.elem.innerHTML = `
-        <section data-component="app">
-        <h1>App Component</h1>  
-        <p>The time is: ${(new Date()).toLocaleString()}</p>
-        </section>
-        `
+        //if (this.elem) this.elem.innerHTML = `
+        //<section data-component="app">
+        //<h1>App Component</h1>  
+        //<p>The time is: ${(new Date()).toLocaleString()}</p>
+        //</section>
+        //`
+        
     }
 }
---
+++ GradiPage/src/helpers/services.js
@@ -0,0 +1,6 @@
+import axios from 'axios';  
+import information from "../../../api/gradiAuthors.json";
+
+export function getData(){
+  return information
+}
